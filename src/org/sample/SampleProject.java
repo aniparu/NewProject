3af882ff3e71512ae79eb65e00c7a126b61e2dad
aniparu <aniparu122@gmail.com>
2022-10-28 16:36:27 +0530

    This is new Project

---
+++ src/org/sample/SampleProject.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+public class SampleProject {
+	private void empId() {
+		System.out.println("employee id 34567");
+
+	}
+	public static void main(String[] args) {
+		SampleProject  sp = new SampleProject();
+		sp.empId();
+	}
+
+}
